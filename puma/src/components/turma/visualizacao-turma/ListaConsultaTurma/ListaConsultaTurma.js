0932e5438aea7a46f772b2c73c401ce586715488
Nilo Mendonça <nilojrmendonca@gmail.com>
2023-01-09 08:42:42 -0300

    Adjustments in the view of classes

--- puma/src/components/turma/visualizacao-turma/ListaConsultaTurma/ListaConsultaTurma.js
+++ puma/src/components/turma/visualizacao-turma/ListaConsultaTurma/ListaConsultaTurma.js
@@ -1,25 +1,31 @@
+import ClassService from '../../../../services/ClassService';
+import SubjectService from '../../../../services/SubjectService';
+
 export default {
+  beforeMount() {
+    this.getData();
+  },
+
   props: {
-    title: String,
-    dataSubjects: Array,
+    mySubjects: Boolean,
     subjectSearch: String,
   },
 
   data: () => ({
     listSubjects: [],
+    listClassesEditable: [],
+    listClasses: [],
+    classService: new ClassService(),
+    subjectService: new SubjectService(),
   }),
 
   watch: {
-    dataSubjects() {
-      this.listSubjects = this.dataSubjects;
-    },
-
     subjectSearch() {
       if (this.subjectSearch) {
-        this.listSubjects = this.dataSubjects.filter((item) => (
-          item.name.toLowerCase().includes(this.subjectSearch.toLowerCase())));
+        this.listClassesEditable = this.listClasses.filter((item) => (
+          item.subject.name.toLowerCase().includes(this.subjectSearch.toLowerCase())));
       } else {
-        this.listSubjects = this.dataSubjects;
+        this.listClassesEditable = this.listClasses;
       }
     },
   },
@@ -27,6 +33,47 @@ export default {
   methods: {
     goToSubject(id) {
       this.$router.push({ path: `/turma/${id}` });
+    },
+    formatSemester(semester) {
+      switch (semester) {
+        case '1':
+          return '1°';
+        case '2':
+          return '2°';
+        case 'VERAO':
+          return 'Verão';
+        default:
+          return '-';
+      }
+    },
+    getData() {
+      this.$store.commit('OPEN_LOADING_MODAL', { title: 'Carregando...' });
+      this.subjectService.getSubjects().then((response) => {
+        this.listSubjects = response.data;
+        this.classService.getClasses().then((res) => {
+          this.listClasses = res.data;
+          for (let i = 0; i < this.listClasses.length;) {
+            for (let j = 0; j < this.listSubjects.length;) {
+              if (this.listSubjects[j].subjectid === this.listClasses[i].subjectid) {
+                this.listClasses[i].subject = this.listSubjects[j];
+                break;
+              }
+              j += 1;
+            }
+            i += 1;
+          }
+          this.listClassesEditable = this.listClasses;
+          this.$store.commit('CLOSE_LOADING_MODAL');
+        });
+      }).catch(() => {
+        this.$store.commit('CLOSE_LOADING_MODAL');
+        this.makeToast('ERRO', 'Erro ao recuperar turmas', 'danger');
+      });
+    },
+    makeToast(toastTitle, toastMessage, toastVariant) {
+      this.$bvToast.toast(toastMessage, {
+        title: toastTitle, variant: toastVariant, solid: true, autoHideDelay: 4000,
+      });
     },
   },
 };
--- puma/src/components/turma/visualizacao-turma/visualizacao-turma.js
+++ puma/src/components/turma/visualizacao-turma/visualizacao-turma.js
@@ -1,27 +1,12 @@
-import SubjectService from '../../../services/SubjectService';
-import ClassService from '../../../services/ClassService';
 import ListaConsultaTurma from './ListaConsultaTurma/ListaConsultaTurma.vue';
 
 export default {
-  beforeMount() {
-    this.getData();
-  },
-
   components: {
     ListaConsultaTurma,
   },
 
   data: () => ({
     subjectSearch: null,
-    isLoading: false,
-    wasLoaded: false,
-    isDeletingSubject: false,
-    subjects: [],
-    mySubjects: [],
-    listClasses: [],
-    subjectService: new SubjectService(),
-
-    classService: new ClassService(),
     dispMySubjects: false,
     dispTimeFIlter: false,
     listYears: [
@@ -38,38 +23,5 @@ export default {
   }),
 
   methods: {
-    getData() {
-      this.$store.commit('OPEN_LOADING_MODAL', { title: 'Carregando...' });
-      this.subjectService.getSubjects().then((response) => {
-        this.subjects = response.data;
-
-        this.classService.getClasses().then((res) => {
-          this.listClasses = res.data;
-          console.log(this.listClasses);
-          this.$store.commit('CLOSE_LOADING_MODAL');
-        });
-      }).catch(() => {
-        this.$store.commit('CLOSE_LOADING_MODAL');
-        this.makeToast('ERRO', 'Erro ao recuperar disciplinas', 'danger');
-      });
-    },
-    separateSubjects() {
-      this.subjects.map((sub) => {
-        sub.professors.map((prof) => {
-          if (prof.userid === this.$store.getters.user.userId) {
-            this.mySubjects.push(sub);
-            this.subjects = this.subjects.filter((item) => (
-              item.subjectid !== sub.subjectid));
-          }
-          return prof;
-        });
-        return null;
-      });
-    },
-    makeToast(toastTitle, toastMessage, toastVariant) {
-      this.$bvToast.toast(toastMessage, {
-        title: toastTitle, variant: toastVariant, solid: true, autoHideDelay: 4000,
-      });
-    },
   },
 };
