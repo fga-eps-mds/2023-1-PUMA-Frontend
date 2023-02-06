d0fe3aaa17983491284cde6a395a313ba3ca6247
giovanadionisio <giovanadionisio.gd@gmail.com>
2023-02-05 23:07:39 -0300

    Add consulta disciplina test
    Co-authored-by: Hérya <hryds@users.noreply.github.com>

---
+++ puma/tests/ConsultaDisicplina.spec.js
@@ -0,0 +1,18 @@
+import ConsultaDisciplina from '../src/components/disciplina/consulta-disciplina/consulta-disciplina'
+import SubjectService from '../src/services/SubjectService';
+
+describe('Get Consulta Disicplina Data', () => {
+    it('Should get Consulta disciplina data', () => {
+        let response = {
+            subjectSearch: null,
+            isLoading: false,
+            wasLoaded: false,
+            isDeletingSubject: false,
+            subjects: [],
+            mySubjects: [],
+            subjectService: new SubjectService(),
+          }
+        expect(ConsultaDisciplina.data()).toStrictEqual(response);
+    })
+  })
+  
