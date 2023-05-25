139595c7151ea1caabe4fdd80c841be9c00cb530
Guilherme Fernandes <guilhermedfs11@gmail.com>
2023-05-25 12:47:57 -0300

    feat: cria tela de ambiente externo Co-authored-by: Pedro Sorroche <pedrosorroche.dev@gmail.com> Co-authored-by: Guilherme Basilio <guilhermebesanto@gmail.com>

--- puma/src/components/contatos/cadastro-contatos/cadastro-contatos.js
+++ puma/src/components/contatos/cadastro-contatos/cadastro-contatos.js
@@ -43,63 +43,8 @@ export default {
         this.$store.commit('CLOSE_LOADING_MODAL');
       }
     },
-    /* async onSubmit() {
-      try {
-        const isFormValid = await this.$refs.observer.validate();
-        const isMultiselectValid = this.validateMultiselects();
-        if (isFormValid && isMultiselectValid) {
-          this.$store.commit('OPEN_LOADING_MODAL', { title: 'Enviando...' });
-          const userType = {
-            userTypeId: this.userTypeId,
-            typeName: this.typeName,
-            description: this.description,
-          };
-          if (this.operacao === 'cadastrar') {
-            this.isLoading = true;
-            this.userService.addUserType(userType).then(async () => {
-              this.isLoading = false;
-              await this.$router.push({ path: '/tipoUsuario' });
-              this.makeToast('SUCESSO', 'Usuário cadastrado com sucesso', 'success');
-              this.$store.commit('CLOSE_LOADING_MODAL');
-            }).catch((error) => {
-              this.isLoading = false;
-              this.makeToast('ERRO', 'Infelizmente houve um erro ao cadastrar o usuário', 'danger');
-              this.$store.commit('CLOSE_LOADING_MODAL');
-            });
-          } else if (this.operacao === 'editar') {
-            this.isLoading = true;
-
-            this.userService.updateUserType(userType).then(async () => {
-              this.isLoading = false;
-              await this.$router.push({ path: '/tipoUsuario' });
-              this.makeToast('SUCESSO', 'Usuário atualizado com sucesso', 'success');
-              this.$store.commit('CLOSE_LOADING_MODAL');
-            }).catch((error) => {
-              this.isLoading = false;
-              this.makeToast('ERRO', 'Infelizmente houve um erro ao atualizar os dados do usuário', 'danger');
-              this.$store.commit('CLOSE_LOADING_MODAL');
-            });
-          }
-        }
-      } catch (error) {
-        this.$store.commit('CLOSE_LOADING_MODAL');
-      }
-    }, */
     makeToast: function (title, message, variant) {
       this.$bvToast.toast(message, { title: title, variant: variant, solid: true, autoHideDelay: 4000 });
     },
     },
-
-    /* deleteUserType(userTypeId) {
-      return new Promise((resolve, reject) => {
-        this.userService.getUserType(userTypeId).then((response) => {
-          resolve();
-          this.$router.push({path: '/tipoUsuario'}).catch(() => {});
-        }).catch((error) => {
-          this.makeToast('ERRO', 'Infelizmente houve um erro ao recuperar os dados do usuário', 'danger');
-          reject();
-        });
-      });
-    }, */
-//   },
 };
