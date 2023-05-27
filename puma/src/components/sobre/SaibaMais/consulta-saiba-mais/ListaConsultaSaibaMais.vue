3c51cdbe613e7a81371b8374965021013e120792
Juliana Valle <juju.pvg@gmail.com>
2023-05-26 17:53:37 -0700

    feat: list view learn more page
    Co-authored-by: Daniel Barcelos <danielbarcelos.7@hotmail.com>
    Co-authored-by: Thiago Mesquita <thiago099carvalho@gmail.com>

---
+++ puma/src/components/sobre/SaibaMais/consulta-saiba-mais/ListaConsultaSaibaMais.vue
@@ -0,0 +1,7 @@
+<template src="./lista-consulta-saiba-mais.html" />
+
+<script src="./lista-consulta-saiba-mais.js"></script>
+
+<style src="./lista-consulta-saiba-mais.css" scoped></style>
+
+<style src="@/assets/css/input.css" scoped></style>
