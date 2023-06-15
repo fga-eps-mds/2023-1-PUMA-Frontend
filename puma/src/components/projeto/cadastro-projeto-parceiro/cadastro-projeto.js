78aa8e40c24e7bdb2d9a1d9d2ad347319aab40f4
Thiago Carvalho <thiago099carvalho@gmail.com>
2023-06-14 22:56:41 -0300

    feat: add cadastro projeto component

---
+++ puma/src/components/projeto/cadastro-projeto-parceiro/cadastro-projeto.js
@@ -0,0 +1,147 @@
+/* eslint-disable */
+import ProjectService from '../../../services/ProjectService';
+import PartnerProjectService from '../../../services/PartnerProjectService';
+import ReturnButton from '../../shared/ReturnButton/ReturnButton.vue';
+
+export default {
+  name: 'CadastroProjeto',
+  components: {
+    ReturnButton,
+  },
+  data() {
+    return {
+      titulo: '',
+      descricao: '',
+      objetivos: '',
+      resultadoEsperado: '',
+      formIsValid: '',
+      keywords: [],
+      mainKeyword: null,
+      selectedKeywords: [],
+      projectService: new ProjectService(),
+      partnerProjectService: new PartnerProjectService(),
+      multiSelectPlaceholder: 'Carregando opções...',
+      projectImages: [],
+      projectImagesUrls: [],
+      pdf: '',
+    };
+  },
+  beforeMount() {
+    // this.getKeywords();
+  },
+  methods: {
+    async onSubmit() {
+      try {
+        const isFormValid = await this.$refs.observer.validate();
+        if (!isFormValid) return;
+
+        const project = {
+          title: this.titulo,
+          problem: this.descricao,
+          expectedResult: this.resultadoEsperado,
+          objectives: this.objetivos,
+          projectImages: this.projectImagesUrls.join('&-&'),
+          projectPdf: this.pdf
+        };
+        await this.partnerProjectService.addProject(project)
+
+        this.makeToast('Projeto cadastrado', `O projeto "${project.name}" foi cadastrado com sucesso`, 'success');
+      } catch (error) {
+        this.$store.commit('CLOSE_LOADING_MODAL');
+        this.makeToast('Falha ao cadastrar projeto', 'Infelizmente houve um erro ao realizar cadastro, confira os dados inseridos e sua conexão com servidor e tente novamente', 'danger');
+      }
+    },
+    async onFileChange(e) {
+      console.log(e)
+      var selectedFiles = e.target.files;
+      console.log(selectedFiles.length)
+      for (let i=0; i < selectedFiles.length; i++)
+      {
+    	  this.getBase64(selectedFiles[i], i);
+	  }
+    console.log(this.projectImagesUrls)
+
+
+    },
+    async onPDFSubmit(e) {
+      console.log(e)
+      var pdfFile = e.target.files[0];
+      console.log(pdfFile)
+      var reader = new FileReader();
+      reader.onload = (e) => {
+        this.pdf = e.target.result;
+      };
+      reader.readAsDataURL(pdfFile)
+    },
+    async getBase64(file, i) {
+      console.log(i)
+      var reader = new FileReader();
+      console.log("file", file)
+      reader.onload = (e) => {
+        this.projectImagesUrls.push(e.target.result);
+      };
+      reader.readAsDataURL(file)
+      reader.onerror = function (error) {
+        console.log('Error: ', error);
+      };
+   }, 
+   
+    removeImage (i) {
+      // alert(i);
+
+    	var arrayImages = this.projectImages;
+    	var index = arrayImages.indexOf(arrayImages[i]);
+		  arrayImages.splice(index, i);
+   
+    },
+    handleChangeKeywords(value) {
+      if (!value.find((k) => k.value === this.mainKeyword?.value)) {
+        this.mainKeyword = null;
+      }
+    },
+    makeToast(title, message, variant) {
+      this.$bvToast.toast(message, {
+        title, variant, solid: true, noAutoHide: true, appendToast: true,
+      });
+    },
+    isChecked(option) {
+      return this.selectedKeywords.some((op) => op.value === option.value);
+    },
+    disableForm() {
+      const inputs = document.getElementsByTagName('input');
+      const textareas = document.getElementsByTagName('textarea');
+      for (let i = 0; i < inputs.length; i += 1) { inputs[i].disabled = true; }
+      for (let i = 0; i < textareas.length; i += 1) { textareas[i].disabled = true; }
+    },
+    removeDropdownIcons() {
+      document.getElementsByClassName('multiselect__select')[0].remove();
+    },
+    async getKeywords() {
+      try {
+        this.$store.commit('OPEN_LOADING_MODAL', { title: 'Carregando...' });
+
+        const response = await this.projectService.getAvailableKeywordsToProject();
+        this.keywords = response.data.map((k) => ({ value: k.keywordid, text: k.keyword }))
+          .sort((a, b) => a.text.localeCompare(b.text));
+        this.multiSelectPlaceholder = this.keywords.length ? 'Selecione' : 'Sem palavras disponíveis';
+
+        this.$store.commit('CLOSE_LOADING_MODAL');
+      } catch (error) {
+        this.multiSelectPlaceholder = 'Sem palavras disponíveis';
+        this.$store.commit('CLOSE_LOADING_MODAL');
+        this.makeToast('Erro de busca', 'Infelizmente houve um erro ao recuperar lista de palavras-chave, confira sua conexão com servidor e tente novamente', 'danger');
+      }
+    },
+    getProject(projectId) {
+      this.projectService.getProject(projectId).then((response) => {
+        const project = response.data;
+        this.selectedKeywords = project.keywords;
+        this.titulo = project.name;
+        this.descricao = project.problem;
+        this.resultadoEsperado = project.expectedresult;
+      }).catch(() => {
+        this.makeToast('Erro de busca', 'Infelizmente houve um erro ao recuperar projeto, confira sua conexão com servidor e tente novamente', 'danger');
+      });
+    },
+  },
+};
