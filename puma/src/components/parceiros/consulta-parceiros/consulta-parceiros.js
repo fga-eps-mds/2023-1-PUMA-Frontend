d0ddb76860541e8220ca14d4961ebbe2f88d4f01
Thiago Carvalho <thiago099carvalho@gmail.com>
2023-07-07 00:59:29 -0300

    feat: partner list
    Co-authored-by: artmarmocathala <arthurmarmocathala@gmail.com>
    Co-authored-by: joaombc <jpceva@gmail.com>

--- puma/src/components/parceiros/consulta-parceiros/consulta-parceiros.js
+++ puma/src/components/parceiros/consulta-parceiros/consulta-parceiros.js
@@ -1,9 +1,12 @@
+/* eslint-disable */
 import SubjectService from '../../../services/SubjectService';
+import PartnerService from '../../../services/PartnerService';
 import ListaParceiros from './ListaParceiros/ListaParceiros.vue';
 
 export default {
+
   beforeMount() {
-    this.getSubjects();
+    this.getPartners();
   },
 
   components: {
@@ -17,32 +20,20 @@ export default {
     isDeletingSubject: false,
     subjects: [],
     mySubjects: [],
+    partnerService: new PartnerService(),
+    partners: [],
+    listSubjects: [],
     subjectService: new SubjectService(),
   }),
 
   methods: {
-    getSubjects() {
-      this.$store.commit('OPEN_LOADING_MODAL', { title: 'Carregando...' });
-      this.subjectService.getSubjects().then((response) => {
-        this.subjects = response.data;
-        this.$store.commit('CLOSE_LOADING_MODAL');
-        this.separateSubjects();
-      }).catch(() => {
-        this.$store.commit('CLOSE_LOADING_MODAL');
-        this.makeToast('Erro de busca', 'Erro ao recuperar a lista de disciplinas, confira sua conexão com servidor e tente novamente', 'danger');
-      });
-    },
-    separateSubjects() {
-      this.subjects.map((sub) => {
-        sub.professors[0].map((prof) => {
-          if (prof.userId === this.$store.getters.user.userId) {
-            this.mySubjects.push(sub);
-            this.subjects = this.subjects.filter((item) => (
-              item.subjectId !== sub.subjectId));
-          }
-          return prof;
-        });
-        return null;
+    getPartners() {
+      this.partnerService.getPartners().then((response) => {
+        this.partners = response.data;
+        console.log(this.partners)
+      }).catch((e) => {
+        console.log(e)
+        this.makeToast('Erro de busca', 'Infelizmente houve um erro ao recuperar lista de parceiros, confira sua conexão com servidor e tente novamente', 'danger');
       });
     },
     makeToast(toastTitle, toastMessage, toastVariant) {
