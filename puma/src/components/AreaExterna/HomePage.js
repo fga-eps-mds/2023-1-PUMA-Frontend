7d88a5c73d04801a22a60ab6d1783ad3c816f509
artmarmocathala <arthurmarmocathala@gmail.com>
2023-07-09 21:37:10 -0700

    feat: show best projects on home page
    Co-authored-by: Juliana Valle <juju.pvg@gmail.com>
    Co-authored-by: Joao Pedro <joaopedroveras310103@gmail.com>

--- puma/src/components/AreaExterna/HomePage.js
+++ puma/src/components/AreaExterna/HomePage.js
@@ -1,11 +1,13 @@
 import AreaExternaHeader from './AreaExternaHeader/AreaExternaHeader.vue';
 import Destaque from './Destaque/Destaque.vue';
+import MelhoresProjetos from './MelhoresProjetos/MelhoresProjetos.vue';
 
 export default {
   name: 'HomePage',
   components: {
     AreaExternaHeader,
     Destaque,
+    MelhoresProjetos,
   },
 
   data() {
